c1a997990e03fa8457ab4c99430f07f1e83278fe
Dionysios Spiliopoulos <32896454+Dspil@users.noreply.github.com>
2020-10-03 18:14:24 +0300

    Add files via upload

---
+++ AutoMail.lua
@@ -0,0 +1,106 @@
+function AutoMail_OnLoad()
+	b = CreateFrame("Button", "MyButton", UIParent, "UIPanelButtonTemplate")
+	b:SetText("Open All")
+	b:SetScript("OnClick", AutoMail)
+	b:Hide()
+end
+
+function AutoMail_EventHandler(event)
+	if event == "MAIL_SHOW" then
+		b:SetParent("InboxFrame")
+		b:SetPoint("TOP", "MailFrame", 0, -b:GetParent():GetHeight() / 12)
+		b:SetSize(b:GetParent():GetWidth() / 4 , b:GetParent():GetHeight() / 20)
+		b:Show()
+	else 
+		b:Hide()
+	end
+
+end
+
+-- open mail logic
+
+automail_control = false
+index = 1
+mailid = 0
+has_changed = false
+start_shown = 0
+
+function OpenAllMail()
+	local shown, total = GetInboxNumItems()
+	if shown < start_shown then
+		has_changed = true
+	end
+	if shown - index + 1 == 0 then
+		automail_control = false
+		InboxGetMoreMail()
+		if has_changed then
+			AutoMail_wait(0.2, AutoMail)
+		end
+		return 0
+	end
+	local packageIcon, stationeryIcon, sender, subject, money, CODAmount, daysLeft, hasItem, wasRead, wasReturned, textCreated, canReply, isGM = GetInboxHeaderInfo(index);
+	if not hasItem then
+		hasItem = 0
+	end
+	if CODAmount > 0 then
+		index = index + 1
+		AutoMail_wait(0, OpenAllMail)
+		return 0
+	end
+	if mailid == daysLeft then
+		if hasItem + money == 0 then
+			DeleteInboxItem(index)
+		else
+			index = index + 1
+		end
+	else
+		mailid = daysLeft
+		AutoLootMailItem(index)
+	end
+	AutoMail_wait(0.05 * (hasItem + 1), OpenAllMail)
+end
+
+function AutoMail()
+	if automail_control then
+		return 0
+	end
+	local shown, total = GetInboxNumItems()
+	start_shown = shown
+	automail_control = true
+	index = 1
+	OpenAllMail()
+end
+
+
+-- wait function below
+
+local waitTable = {};
+local waitFrame = nil;
+
+function AutoMail_wait(delay, func, ...)
+  if(type(delay)~="number" or type(func)~="function") then
+    return false;
+  end
+  if(waitFrame == nil) then
+    waitFrame = CreateFrame("Frame","WaitFrame", UIParent);
+    waitFrame:SetScript("onUpdate",function (self,elapse)
+      local count = #waitTable;
+      local i = 1;
+      while(i<=count) do
+        local waitRecord = tremove(waitTable,i);
+        local d = tremove(waitRecord,1);
+        local f = tremove(waitRecord,1);
+        local p = tremove(waitRecord,1);
+        if(d>elapse) then
+          tinsert(waitTable,i,{d-elapse,f,p});
+          i = i + 1;
+        else
+          count = count - 1;
+          f(unpack(p));
+        end
+      end
+    end);
+  end
+  tinsert(waitTable,{delay,func,{...}});
+  return true;
+end
